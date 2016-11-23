005f527425ad3907fa525e30eb41b018d50bf04b
Torsten Houwaart <houwaart@informatik.uni-freiburg.de>
2016-11-23 23:57:11 +0100

    Updated script.

--- common_pos_in_bed.py
+++ common_pos_in_bed.py
@@ -17,7 +17,10 @@ with open (args.file1) as f:
         end = int(sline[2])
         for i in range(beg,end+1):
             key = sline[0]+"_"+sline[5].rstrip()+"_"+str(i)
-            pos[key] = "1"
+            if key in pos:
+                pos[key] |= 0b100
+            else:
+                pos[key] = 0b100
             
 with open (args.file2) as f:
     for line in f:
@@ -26,10 +29,10 @@ with open (args.file2) as f:
         end = int(sline[2])
         for i in range(beg,end+1):
             key = sline[0]+"_"+sline[5].rstrip()+"_"+str(i)
-            if pos[key]:
-                pos[key] += "2"
+            if key in pos:
+                pos[key] |= 0b010
             else:
-                pos[key] = "2"
+                pos[key] = 0b010
 
 with open (args.file3) as f:
     for line in f:
@@ -38,10 +41,10 @@ with open (args.file3) as f:
         end = int(sline[2])
         for i in range(beg,end+1):
             key = sline[0]+"_"+sline[5].rstrip()+"_"+str(i)
-            if pos[key]:
-                pos[key] += "3"
+            if key in pos:
+                pos[key] |= 0b001
             else:
-                pos[key] = "3"
+                pos[key] = 0b001
 
 allpos = 0.0
 in1 = 0.0
@@ -53,37 +56,27 @@ in23 = 0.0
 in123 = 0.0
 for key,value in pos.iteritems():
     allpos += 1
-    if value == "1":
+    if value == 0b100:
         in1 += 1
-    if value == "2":
-        in1 += 2
-    if value == "3":
-        in1 += 3
-    elif value == "12":
-        in1 += 1
-        in2 += 1
+    elif value == 0b010:
+        in2 += 2
+    elif value == 0b001:
+        in3 += 3
+    elif value == 0b110:
         in12 += 1
-    elif value == "13":
-        in1 += 1
-        in3 += 1
+    elif value == 0b101:
         in13 += 1
-    elif value == "23":
-        in2 += 1
-        in3 += 1
+    elif value == 0b011:
         in23 += 1
-    elif value == "123":
-        in1 += 1
-        in2 += 1
-        in3 += 1
-        in12 += 1
-        in13 += 1
-        in23 += 1
+    elif value == 0b111:
         in123 += 1
+    else :
+        print key + value
 
-print "in1: " + str(in1/allpos)
-print "in2: " + str(in2/allpos)
-print "in3: " + str(in3/allpos)
-print "in12: " + str(in12/allpos)
-print "in13: " + str(in13/allpos)
-print "in23: " + str(in23/allpos)
-print "in123: " + str(in123/allpos)
+print "in1: " + str(in1) + "\t" + str(in1/allpos)
+print "in2: " +  str(in2) + "\t" + str(in2/allpos)
+print "in3: " +  str(in3) + "\t" + str(in3/allpos)
+print "in12: " + str(in12) + "\t" +  str(in12/allpos)
+print "in13: " + str(in13) + "\t" +  str(in13/allpos)
+print "in23: " + str(in23) + "\t" +  str(in23/allpos)
+print "in123: " + str(in123) + "\t" +  str(in123/allpos)
