6ca1c21da216b11b8e5d06db495cf5aacf9d22ac
torhou <toho@gmx.de>
2021-03-12 09:33:56 +0100

    Some improvementes

--- pairwise_sequence_alignments.py
+++ pairwise_sequence_alignments.py
@@ -12,6 +12,7 @@ import re
 parser = ArgumentParser()
 parser.add_argument("infile") 
 parser.add_argument("reffile") 
+parser.add_argument("--model", default="strict", help="Choose either [strict] or lenient")
 args = parser.parse_args()
 
 refseq = ""
@@ -22,19 +23,30 @@ addedEquals=[("N", "A"), ("N", "G"),("N","T"),("N","C")]
 
 
 
-for record in SeqIO.parse(args.reffile, "fasta"):
-    refidx = record.id
-    seqs[record.id] = str(record.seq).upper()
+for nr, record in enumerate(SeqIO.parse(args.reffile, "fasta")):
+    if nr > 0:
+        print("Warning: More than one sequence was found in " + args.reffile)
+        print("Only the first entry will be used as reference.")
+        break
+    refidx = record.description
+    seqs[record.description] = str(record.seq).upper()
 
 
 for record in SeqIO.parse(args.infile, "fasta"):
-    seqs[record.id] = str(record.seq).upper()
+    seqs[record.description] = str(record.seq).upper()
 
-# Calculation of distances, if necessary
 for i1 in seqs.keys() - set([refidx]):
     s1, s2 = [seqs[i1],seqs[refidx]]
-    res = edlib.align(s1, s2,task='path',additionalEqualities=addedEquals)
+    print(i1)
+    if args.model == "strict":
+        res = edlib.align(s1, s2,task='path',additionalEqualities=addedEquals)
+    elif args.model == "lenient":
+        res = edlib.align(s1, s2,task='path',additionalEqualities=addedEquals2)
+    else:
+        print("Model must be either strict or lenient.")
+        sys.exit()
     print(res)
+    #print(str(i1) + "\t" + str(res))
 
 
 
