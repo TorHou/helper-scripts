f29253249f86ae3eea7d0efd1921316ae93fe51c
torhou <toho@gmx.de>
2021-09-28 14:05:07 +0200

    I keep losing this script

---
+++ subset_fasta.py
@@ -0,0 +1,19 @@
+import argparse
+from Bio import SeqIO
+
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument("inputfile")
+arg_parser.add_argument("contig")
+arg_parser.add_argument("start", type=int)
+arg_parser.add_argument("end", type=int)
+
+args=arg_parser.parse_args()
+
+
+with open(args.inputfile) as inf:
+    for record in SeqIO.parse(inf,'fasta'):
+        if args.contig == record.id:
+            print(record.seq[args.start:args.end])
+
+
