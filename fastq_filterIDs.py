df9750d88e220358d17d1e299150bdad9ca02ed5
torhou <toho@gmx.de>
2021-12-15 19:26:10 +0100

    Double the funtionality

--- fastq_filterIDs.py
+++ fastq_filterIDs.py
@@ -5,18 +5,22 @@ arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument("fastqfile")
 arg_parser.add_argument("idfile")
+arg_parser.add_argument("--deplete", default=False, action="store_true")
 
 args=arg_parser.parse_args()
 
-ids={}
+ids=set()
 
 with open(args.idfile) as f:
     for line in f:
-        ids[line.rstrip()]=1
+        rid = line.rstrip().split()[0]
+        ids.add(rid)
 
 fastq_sequences = SeqIO.parse(open(args.fastqfile),'fastq')
 
 
 for fastq in fastq_sequences:
-    if fastq.id in ids:
-        print fastq.format("fastq").rstrip()
+    if deplete:
+        if fastq.id not in ids: print(fastq.format("fastq").rstrip())
+    else: 
+        if fastq.id in ids: print(fastq.format("fastq").rstrip())
